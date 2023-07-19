7621d819fa79d00ed7a557c168df860543a33623
Aceruling <aceruling@gmail>
2023-07-19 19:09:35 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/Home/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
