1cd98463243ab3290c8b3cc79b85c9c3980c0827
Aceruling <aceruling@gmail.com>
2023-07-20 16:56:13 +0900

    modify sign in | get started button

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,42 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import "./Header.css";
-import { getConfig } from "../../../config";
+// import { getConfig } from "../../../config";
 import { Auth0WalletConnector } from '@zerodevapp/wagmi';
-import { configureChains, useAccount, useConnect, useDisconnect, useNetwork } from "wagmi";
+import { configureChains, useAccount, useConnect, useDisconnect } from "wagmi";
 import { publicProvider } from 'wagmi/providers/public'
 import { goerli } from 'wagmi/chains'
+import { shortenAddress } from "../../../helper";
+
 
 const Header = () => {
   const [menuCollapse, setMenuCollapse] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
-
-  const config = getConfig();
-  const { chains } = configureChains( [goerli], [publicProvider()] );
-  const auth0Connector = new Auth0WalletConnector({chains, options: {
-    // projectId: config.projectId,
-    projectId: 'b5486fa4-e3d9-450b-8428-646e757c10f6',
-  }})
-  console.log(auth0Connector)
-
-
-  const [loading, setLoading] = useState(false)
-  const { connect, error, isLoading, pendingConnector } = useConnect()
-  const { address, connector, isConnected } = useAccount()
-  const { disconnect } = useDisconnect()
-  const { chain } = useNetwork()
-
-  const connectWallet = async () => {
-    setLoading(true)
-    await connect({
-        connector: auth0Connector
-    })
-    setLoading(false)
-  }
-
-  //create a custom function that will change menucollapse state from false to true and true to false
   const menuIconClick = () => {
-    //condition checking to change state from true to false and vice versa
     menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
   };
   const sideBarStyle = {
@@ -51,6 +27,33 @@ const Header = () => {
     zIndex: menuCollapse ? "1" : "0",
   };
 
+  // const config = getConfig();
+  const { chains } = configureChains( [goerli], [publicProvider()] );
+  const auth0Connector = new Auth0WalletConnector({chains, options: {
+    // projectId: config.projectId,
+    projectId: 'b5486fa4-e3d9-450b-8428-646e757c10f6',
+  }})
+
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false)
+  const { connect, isLoading } = useConnect()
+  const { address, isConnected } = useAccount()
+  const { disconnect } = useDisconnect()
+
+  const connectWallet = async () => {
+    setLoading(true)
+    await connect({
+        connector: auth0Connector
+    })
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    isConnected ? 
+      navigate('/startloan') : 
+      navigate('/');
+  }, [isConnected]);
+
   return (
     <div>
       <div style={sideBarStyle} className="sidebar">
@@ -64,12 +67,19 @@ const Header = () => {
             <div>FAQ</div>
           </div>
           <div className="signIn">
-            <div>Sign in</div>
-            <div>|</div>
-            <div>Get start</div>
+            {isConnected ? 
+              <button className="signInButton" onClick={disconnect}>
+                {/* {shortenAddress(address)} | Disconnect */}
+                {address} | Disconnect
+              </button>
+              :
+              <button className="signInButton" disabled={isLoading || loading} onClick={connectWallet}>
+                Sign in | Get started
+              </button>
+            }
           </div>
           <div className="sidebarOpenBtn" onClick={menuIconClick}>
-            <img src="./assets/icons/menu.png" className="menuIconImg" />{" "}
+            <img src="./assets/icons/menu.png" alt="menu" className="menuIconImg" />{" "}
           </div>
         </div>
       </div>
