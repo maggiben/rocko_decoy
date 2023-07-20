6076244bfb49065e1f3b3baea2c245a224d345c2
Aceruling <aceruling@gmail.com>
2023-07-20 15:11:15 +0900

    modify header

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,9 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import "./Header.css";
+import { getConfig } from "../../../config";
+import { Auth0WalletConnector } from '@zerodevapp/wagmi';
+import { configureChains, useAccount, useConnect, useDisconnect, useNetwork } from "wagmi";
+import { publicProvider } from 'wagmi/providers/public'
+import { goerli } from 'wagmi/chains'
+
 const Header = () => {
   const [menuCollapse, setMenuCollapse] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
+
+  const config = getConfig();
+  const { chains } = configureChains( [goerli], [publicProvider()] );
+  const auth0Connector = new Auth0WalletConnector({chains, options: {
+    // projectId: config.projectId,
+    projectId: 'b5486fa4-e3d9-450b-8428-646e757c10f6',
+  }})
+  console.log(auth0Connector)
+
+
+  const [loading, setLoading] = useState(false)
+  const { connect, error, isLoading, pendingConnector } = useConnect()
+  const { address, connector, isConnected } = useAccount()
+  const { disconnect } = useDisconnect()
+  const { chain } = useNetwork()
+
+  const connectWallet = async () => {
+    setLoading(true)
+    await connect({
+        connector: auth0Connector
+    })
+    setLoading(false)
+  }
 
   //create a custom function that will change menucollapse state from false to true and true to false
   const menuIconClick = () => {
@@ -21,6 +50,7 @@ const Header = () => {
     borderRadius: "10px",
     zIndex: menuCollapse ? "1" : "0",
   };
+
   return (
     <div>
       <div style={sideBarStyle} className="sidebar">
@@ -34,9 +64,12 @@ const Header = () => {
             <div>FAQ</div>
           </div>
           <div className="signIn">
-            <div>Sign in</div>
-            <div>|</div>
-            <div>Get start</div>
+            {isConnected ? 
+              <button className="signInButton" onClick={disconnect}>Disconnect</button>  
+              :            
+              <button className="signInButton" disabled={isLoading || loading} onClick={connectWallet}>
+                Sign in | Get started
+              </button>}
           </div>
           <div className="sidebarOpenBtn" onClick={menuIconClick}>
             <img src="./assets/icons/menu.png" className="menuIconImg" />{" "}
