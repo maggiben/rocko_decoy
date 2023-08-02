448d549a574b83639e18965053743d6a75af0b03
Aceruling <aceruling@gmail.com>
2023-08-02 16:54:08 +0900

    modify progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,39 +1,53 @@
 import React, { useState } from "react";
 import "./ProgressBar.css";
-const ProgressBar = ({ step, min, max }) => {
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(1000);
-  const handleMinChange = (event) => {
-    event.preventDefault();
-    console.log(event.target.value);
-    const value = parseFloat(event.target.value);
-    const newMinVal = Math.min(value, maxValue);
-    setMinValue(newMinVal);
-  };
+// import 'bootstrap/dist/css/bootstrap.css'; // or include from a CDN
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import RangeSlider from 'react-bootstrap-range-slider';
 
+const ProgressBar = ({setBufferCollateral, bufferCollateral}) => {
   return (
-    <div style={{ width: "85%" }}>
-      <input
-        type="range"
-        value={minValue}
-        min={0}
-        max={1000}
-        step={step}
-        onChange={handleMinChange}
-        style={{
-          width: "100%",
-          height: "25px",
-          borderRadius: "10px",
-          // background: "#05B516",
-          outline: "none",
-          opacity: "0.7",
-          transition: ".2s",
-          hover: {
-            opacity: "1",
-          },
-        }}
-      />
-    </div>
+    <>
+      <div>
+        <RangeSlider
+          style={{
+            width: "441px",
+            height: "15px"
+          }}
+          value={bufferCollateral}
+          onChange={e => {setBufferCollateral(e.target.value)}}
+          min={0}
+          max={500}
+          step={100}
+        />
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <span>Higher risk</span>
+          <span>{bufferCollateral}%</span>
+          <span>Lower risk</span>
+        </div>
+      </div>
+    </>
+    // <div style={{ width: "85%" }}>
+    //   <input
+    //     type="range"
+    //     value={minValue}
+    //     min={0}
+    //     max={1000}
+    //     step={step}
+    //     onChange={handleMinChange}
+    //     style={{
+    //       width: "100%",
+    //       height: "25px",
+    //       borderRadius: "10px",
+    //       // background: "#05B516",
+    //       outline: "none",
+    //       opacity: "0.7",
+    //       transition: ".2s",
+    //       hover: {
+    //         opacity: "1",
+    //       },
+    //     }}
+    //   />
+    // </div>
   );
 };
 
