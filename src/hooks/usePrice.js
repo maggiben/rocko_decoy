1ba364e4a554c735fb6eaa1398006a0f94660c45
Aceruling <aceruling@gmail.com>
2023-08-11 02:51:27 +0900

    add comp price from coinbase

--- src/hooks/usePrice.js
+++ src/hooks/usePrice.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback } from 'react'
 import axios from "axios"
 import { financial } from '../helper'
 
-const usePrice = () => {
+export const useETHPrice = () => {
     const [ ethprice, setEthPrice ] = useState()
 
     const getCurrentEthPrice = useCallback(async () => {
@@ -23,4 +23,23 @@ const usePrice = () => {
     return { ethprice }
 }
 
-export default usePrice
+export const useCompPrice = () => {
+    const [ compprice, setCompPrice ] = useState()
+
+    const getPrice = useCallback(async () => {
+            const response = await axios.get('https://api.coinbase.com/v2/prices/COMP-USD/spot');
+            
+            if (response.status === 200) {
+                const price = response.data.data.amount
+                setCompPrice(financial(price, 2))
+            } else {
+                setCompPrice(0)
+            }
+    }, [ setCompPrice ])
+
+    useEffect(() => {
+        getPrice()
+    })
+
+    return { compprice }
+}
