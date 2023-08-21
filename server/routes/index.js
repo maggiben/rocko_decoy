fa2a9671def7d6bfd2fab8aaba3b4699e7f3ca03
Jason Camp <jason@readyset.io>
2023-08-21 09:55:50 -0400

    Added /healthcheck for load balancer health check and Dockerfile with helper scripts

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // Import the specific routers
 const loanRouter = require('./loan');
 const coinbaseRouter = require('./coinbase');
+const healthcheckRouter = require('./healthcheck');
 
 router.use('/', coinbaseRouter);
 router.use('/', loanRouter);
+router.use('/', healthcheckRouter);
 
 module.exports = router;
