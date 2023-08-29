4d1b7e11cad60eac388dffdf4185fe6f0259ce8a
Tomasz Zborek <tomaszadaminsider@gmail.com>
2023-08-29 16:01:45 -0400

    all screens finished

---
+++ designs/src/components/pages/Dashboard/toggleBTN/toggleBTN.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+
+export default function ToggleBTN() {
+  const [enabled, setEnabled] = useState(true);
+
+  return (
+    <div className="md:mt-1">
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${enabled ? "bg-black" : "bg-gray-300"}
+          relative inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+      >
+        <span
+          aria-hidden="true"
+          className={`${enabled ? "translate-x-[20px]" : "translate-x-0"}
+            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+        />
+      </Switch>
+    </div>
+  );
+}
