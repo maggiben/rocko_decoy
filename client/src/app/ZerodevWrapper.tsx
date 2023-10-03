a4107198cb20dc1e682caefb527cba96ad5914cc
Aceruling <aceruling@gmail.com>
2023-10-04 01:13:22 +0900

    add infurakey

--- client/src/app/ZerodevWrapper.tsx
+++ client/src/app/ZerodevWrapper.tsx
@@ -11,7 +11,7 @@ import { NETWORK } from "../constants/env";
 const net = (chains as { [key: string]: any })[NETWORK];
 export const { publicClient, webSocketPublicClient } = configureChains(
   [net],
-  [infuraProvider({apiKey: process.env.REACT_APP_INFURA_APIKEY || ""})]
+  [infuraProvider({apiKey: process.env.REACT_APP_INFURA_APIKEY || "fde85b66d55f44e0bb36be6c88c7f1c3"})]
 )
 
 const config = createConfig({
