0a6c978e9b2d928f705f58ff44ca843361c4b87a
Vincent James <2112033+vinniejames@users.noreply.github.com>
2023-10-04 12:55:55 -0500

    addd demo flag

--- client/src/constants/env.ts
+++ client/src/constants/env.ts
@@ -3,4 +3,12 @@ export const COINBASE_CLIENT_ID = process.env.REACT_APP_COINBASE_CLIENT_ID;
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const NETWORK = process.env.REACT_APP_NETWORK || "goerli";
 export const INFURA_APIKEY = process.env.REACT_APP_INFURA_APIKEY || "fde85b66d55f44e0bb36be6c88c7f1c3";
-export const IS_DEMO_MODE = process.env.REACT_APP_DEMO_MODE || true;
+
+const params = new URLSearchParams(window.location.search);
+const demoMode = params.get('demo_mode');
+if (demoMode) {
+  sessionStorage.setItem('IS_DEMO_MODE', 'true');
+  console.log('DEMO MODE ENABLED');
+}
+
+export const IS_DEMO_MODE = process.env.REACT_APP_IS_DEMO_MODE === 'true' || !!demoMode || !!sessionStorage.getItem('IS_DEMO_MODE');
