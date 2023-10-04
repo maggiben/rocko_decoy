9e0eb802ba941d88ddac3426d050b8184f9c4a09
Aceruling <aceruling@gmail.com>
2023-10-04 18:38:21 +0900

    modify buffer page and fetch data based on NETWORK var

--- client/src/components/pages/Steps/StepFour/StepFour.tsx
+++ client/src/components/pages/Steps/StepFour/StepFour.tsx
@@ -1,24 +1,26 @@
-import { FC, useState, useEffect, useRef } from "react";
+import { FC, ChangeEvent, FocusEvent, useState, useEffect, useRef } from "react";
 import { RiskStep } from "@/types/type";
 import Link from "next/link";
 import LoanSummary from "@/components/chips/LoanSummary/LoanSummary";
 import useLoanData from "@/hooks/useLoanData";
-import { useSingleLoan } from "@/contract/single";
+import financial from "@/utility/currencyFormate";
 
 const StepFour: FC<RiskStep> = ({ title }) => {
-  const [value, setValue] = useState(0);
-  const [thumbPosition, setThumbPosition] = useState(0);
-  const [valueDivWidth, setValueDivWidth] = useState(0);
+  const [value, setValue] = useState<number>(0);
+  const [customValue, setCustomValue] = useState<number>();
+  const [thumbPosition, setThumbPosition] = useState<number>(0);
+  const [valueDivWidth, setValueDivWidth] = useState<number>(0);
+  
+  const [minimum, setMinimum] = useState<number>(0);
   const {loanData,setLoanData} = useLoanData()
-  const {
-    getETHPrice,
-    getLTV,
-    getThreshold,
-  } = useSingleLoan();
   
   const valueDivRef = useRef<HTMLDivElement | null>(null);
 
   const initialize = () => {
+    // for start
+    setMinimum(loanData?.collateralNeeded / (1 + loanData?.buffer / 100));
+
+    // for keeping status
     if (loanData?.buffer !== 0 && loanData?.buffer) {
       setValue(loanData?.buffer);
       setThumbPosition(
@@ -38,9 +40,9 @@ const StepFour: FC<RiskStep> = ({ title }) => {
 
   const updateLoanData = async (newBuffer: number) => {
     try {
-      const loanToValue: any = await getLTV();
-      const threshold: any = await getThreshold();
-      const ethPrice = await getETHPrice();
+      const loanToValue: any = loanData?.loanToValue;
+      const threshold: any = loanData?.liquidationThreshold;
+      const ethPrice = loanData?.collateralPrice;
       const collateralInUSD = loanData?.borrowing ? loanData?.borrowing / loanToValue * (1 + newBuffer / 100) : 0;
       const collateral = collateralInUSD / ethPrice;
       const liquidationPrice = loanData?.borrowing ? loanData?.borrowing / threshold / collateral : 0;
@@ -64,6 +66,7 @@ const StepFour: FC<RiskStep> = ({ title }) => {
   const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = parseInt(event.target.value, 10);
     setValue(newValue);
+    setCustomValue(newValue);
     setThumbPosition(
       ((newValue - parseInt(event.target.min, 10)) /
         (parseInt(event.target.max, 10) - parseInt(event.target.min, 10))) *
@@ -73,7 +76,55 @@ const StepFour: FC<RiskStep> = ({ title }) => {
     if(setLoanData)
       await updateLoanData(newValue);
   };
-  
+
+  const handleCustomInputBlur = (event: FocusEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value || '0', 10);
+    if (newValue <= 1000) {
+      setValue(newValue);
+    }
+    if (newValue > 1000) {
+      setCustomValue(1000);
+      setValue(1000);
+      return;
+    }
+    if (setLoanData) {
+      setLoanData((prevLoanData) => ({
+        ...prevLoanData,
+        activeNextButton: true,
+      }));
+    }
+
+    if (newValue > 400) return;
+    setThumbPosition(
+      ((newValue - parseInt(event.target.min, 10)) /
+        (400 - parseInt(event.target.min, 10))) *
+        100
+    );
+  };
+  const handleCustomInputChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      setCustomValue(undefined);
+      return;
+    }
+    const newValue = parseInt(event.target.value, 10);
+    if (newValue <= 1000) {
+      setCustomValue(newValue);
+    }
+
+    if (newValue > 1000) {
+      setCustomValue(1000);
+      setValue(1000);
+    }
+
+    if (newValue >= 400 && newValue <= 1000) {
+      setValue(newValue);
+    }
+
+    if(setLoanData)
+      await updateLoanData(newValue > 1000 ? 1000 : newValue);    
+  };
+
   const isEnd = thumbPosition === 100 ;
     const adjustedThumbPosition = isEnd
       ? thumbPosition - (valueDivWidth / 2)  // Subtract half of value div width
@@ -108,42 +159,110 @@ const StepFour: FC<RiskStep> = ({ title }) => {
               {title}
             </p>
 
-            <div className="flex items-end justify-between gap-3 min-h-[160px] py-4">
-              <p className="">Higher Risk</p>
+            <div className="flex items-center justify-between gap-1 md:gap-3 mt-20 md:mt-[144px]  py-4">
+              <p className="text-sm md:text-base">Higher Risk</p>
 
-              <div className="relative flex-1">
-                <input
-                  type="range"
-                  className="range w-full"
-                  min="0"
-                  max="400"
-                  value={value}
-                  onChange={handleInputChange}
-                  data-value={value}
-                  style={{
-                    background: `linear-gradient(to right, #2C3B8D 0%, #2C3B8D ${thumbPosition}%, #E2E2E2 ${thumbPosition}%, #E2E2E2 100%)`,
-                  }}
-                />
-                <div
-                ref={valueDivRef}
-                  className="absolute w-20 h-12 left-0 -top-16 text-center text-white bg-[#2C3B8D] rounded-full py-3 px-4"
-                  style={valueDivStyle}
-                >
-                  <p className="">{value}%</p>
-                 {/*  <div className="w-1 h-10 bg-[#2C3B8D] absolute top-full left-1/2 -translate-x-1/2"></div> */}
+              <div className="flex-1 flex items-start">
+                <div className="w-10 md:w-20 h-[6px]"></div>
+
+                {/* fake bar space of right */}
+                <div className=" flex-1">
+                  {/* rang slider container */}
+                  <div className="relative w-full">
+                    {/* fake bar start */}
+                    <div className="w-10 md:w-20 h-[6px] rounded-full bg-[#2C3B8D] mt-[3px] absolute top-1/2 -translate-y-1/2 right-[calc(100%-4px)]">
+                      <div className="frame h-3 w-3 bg-[#2C3B8D] rotate-180 absolute -top-2  -right-3"></div>
+                      <div className="frame h-3 w-3 bg-[#2C3B8D] absolute top-[2px] -right-3"></div>
+                    </div>
+                    {/* fake bar end */}
+                    {/* initial fix value start */}
+                    <div className="flex items-center justify-between w-fit absolute  top-10 -left-14 ">
+                      <div className="text-center ">
+                        <p className="text-[#545454] text-xs whitespace-nowrap">
+                          Minimum collateral required:
+                        </p>
+                        <p className="text-[#141414] text-xs whitespace-nowrap">
+                          {financial(minimum, 4)} ETH (${financial(loanData?.collateralPrice * minimum, 2)})
+                        </p>
+                      </div>
+                    </div>
+                    {/* initial fix value end */}
+                    <input
+                      type="range"
+                      className="range w-full "
+                      min="0"
+                      max="400"
+                      value={value}
+                      onChange={handleInputChange}
+                      data-value={value}
+                      style={{
+                        background: `linear-gradient(to right, #2C3B8D 0%, #2C3B8D ${thumbPosition}%, #E2E2E2 ${thumbPosition}%, #E2E2E2 100%)`,
+                      }}
+                    />{" "}
+                    <div
+                      ref={valueDivRef}
+                      className="absolute w-20 h-12 left-0 -top-16 text-center text-white bg-[#2C3B8D] rounded-full py-3 px-4"
+                      style={valueDivStyle}
+                    >
+                      <p className="">{value}%</p>
+                      {/*  <div className="w-1 h-10 bg-[#2C3B8D] absolute top-full left-1/2 -translate-x-1/2"></div> */}
+                    </div>
+                  </div>
                 </div>
               </div>
-              <p className="">Lower Risk</p>
+              <p className="text-sm md:text-base">Lower Risk</p>
             </div>
-            <p className="pt-6   text-sm text-blackSecondary">
-            Your loan requires your collateral to maintain a certain value at all times, otherwise your collateral may be liquidated (i.e. sold) by the lender.{" "}
-            <span className="font-bold">A collateral buffer is the percentage of collateral you provide above what is required for your loan.{" "}</span>
-            A larger collateral buffer will reduce the collateral asset price at which your collateral would be liquidated (i.e. liquidation price).{" "}
+            {/* custom field */}
+            {value >= 400 ? (
+              <div className="ml-auto w-fit space-y-1 mt-10 mb-4 md:mt-0">
+                <p className="text-[#141414] font-medium text-xs ">
+                  Enter custom amount
+                </p>
+                <div className="flex items-center gap-2 border border-[#E6E6E6] rounded-[10px] py-2 px-4 w-fit ">
+                  <input
+                    type="number"
+                    name=""
+                    id=""
+                    min="0"
+                    max="1000"
+                    value={customValue}
+                    onBlur={handleCustomInputBlur}
+                    onChange={handleCustomInputChange}
+                    onKeyDown={(event) => {
+                      const keyPressed = event.key;
+                      const isDecimalDigit = /^\d+$/.test(keyPressed);
+                      const isAllowedHexChar = /^[a-eA-E]+$/.test(keyPressed);
+  
+                      if (
+                        (!isDecimalDigit && !(event.key === "Backspace")) ||
+                        isAllowedHexChar
+                      ) {
+                        event.preventDefault();
+                      }
+                      if (event.key === "-") {
+                        event.preventDefault();
+                      }
+                    }}
+                    className="focus:outline-none border-none max-w-[136px] w-full number-input"
+                  />
+                  <p className="text-[#141414] ">%</p>
+                </div>
+              </div>
+            ) : (
+              <div className="h-12 w-full"></div>
+            )}
+            {/* ${value >= 400 ? ' pt-6 ' : 'pt-12'} */}
+
+            <p className={`text-sm text-blackSecondary`}>
+              Your loan requires your collateral to maintain a certain value at
+              all times, otherwise your collateral may be liquidated (i.e. sold)
+              by the lender. By posting more collateral than required, you can
+              reduce the projected liquidation price and the likelihood of this
+              occurring.{" "}
               <Link href={"/"} className="underline">
                 Learn more.
               </Link>
             </p>
-           
           </div>
         </div>
         <div className="p-6 border border-[#E2E2E2] flex-1 rounded-2xl">
--- client/src/constants/env.ts
+++ client/src/constants/env.ts
@@ -3,3 +3,4 @@ export const COINBASE_CLIENT_ID = process.env.REACT_APP_COINBASE_CLIENT_ID;
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const NETWORK = process.env.REACT_APP_NETWORK || "goerli";
 export const INFURA_APIKEY = process.env.REACT_APP_INFURA_APIKEY || "fde85b66d55f44e0bb36be6c88c7f1c3";
+export const IS_DEMO_MODE = process.env.REACT_APP_DEMO_MODE || true;
