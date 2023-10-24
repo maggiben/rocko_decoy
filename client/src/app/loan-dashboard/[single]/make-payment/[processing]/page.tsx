bba49b1d093f5b1300ebfdff1478780781566cc2
Aceruling <aceruling@gmail.com>
2023-10-25 05:39:56 +0900

    fix processing status bar in make-payment

--- client/src/app/loan-dashboard/[single]/make-payment/[processing]/page.tsx
+++ client/src/app/loan-dashboard/[single]/make-payment/[processing]/page.tsx
@@ -213,7 +213,7 @@ const Processing = () => {
   return (
     <main className="container mx-auto px-[15px] py-4 sm:py-6 lg:py-10">
       <h1 className="text-[28px] lg:text-3xl font-medium text-center lg:text-left">
-        Processing Payment
+        {startB ? "Processing Payment" : activeDone ? "Payment Complete" : "Waiting Payment"}
       </h1>
       <section className="my-6">
         <div className="lg:w-3/5 border-2 rounded-2xl p-3 lg:p-6">
