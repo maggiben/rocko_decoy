abff25431e5d49d7ecdf28deafd0c3bea894f9fb
Aceruling <aceruling@gmail.com>
2023-11-08 19:11:33 +0900

    fix again

--- server/routes/loan.js
+++ server/routes/loan.js
@@ -124,10 +124,8 @@ const VPNAPI_KEY = process.env.VPNAPI_KEY;
 router.get('/vpn', async (req, res) => {
   try {
     const ip = req.query.ip;
-    const response = await axios.get(`${VPNAPI_URL}/${ip}`, {
-      key: VPNAPI_KEY
-    });
-    res.send(response)
+    const response = await axios.get(`${VPNAPI_URL}/${ip}?key=${VPNAPI_KEY}`);
+    res.send(response.data)
   } catch (error) {
     console.error('Error call vpnapi:', error);
     res.status(500).send('Failed to call vpn api', error);
