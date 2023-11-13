e23ad407ccdc63b64ea39afd2bc9c4ccca57a059
Jason Camp <me@jasoncamp.com>
2023-11-13 18:47:34 -0500

    backing out site changes

--- infrastructure/terraform/site/us-east-1/cert.tf
+++
@@ -1,8 +0,0 @@
-module "rocko-cert" {
-  source = "../../modules/rocko-cert"
-
-  zone_id     = aws_route53_zone.rocko_cloud.id
-  environment = local.environment
-  domain      = local.domain
-  subdomain   = local.subdomain
-}
