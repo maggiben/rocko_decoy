4e876d3abd2b5a0b1f2bae0c5a0b7bc0b091ab26
Jason Camp <me@jasoncamp.com>
2023-11-14 06:44:43 -0500

    Added missing ecs image var

--- infrastructure/terraform/prod/us-east-1/local.tf
+++ infrastructure/terraform/prod/us-east-1/local.tf
@@ -3,4 +3,9 @@ locals {
   environment = "prod"
   domain      = "rocko.cloud"
   subdomain   = "prod.rocko.cloud"
+
+  ecs_rocko_backend_image = "835780150279.dkr.ecr.us-east-1.amazonaws.com/rocko_backend:dev_latest"
+
+  backend_url = "https://backend.prod.rocko.cloud"
+  client_url  = "https://app.rocko.co"
 }
