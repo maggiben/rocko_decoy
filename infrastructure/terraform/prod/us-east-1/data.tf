ff5269155110bf7dcc2529b798ab5bc84ee9436c
Jason Camp <me@jasoncamp.com>
2023-11-14 06:29:23 -0500

    Added ECS, LB, and DNS for main

--- infrastructure/terraform/prod/us-east-1/data.tf
+++ infrastructure/terraform/prod/us-east-1/data.tf
@@ -15,3 +15,47 @@ data "aws_secretsmanager_secret_version" "provider_url" {
 data "aws_secretsmanager_secret_version" "cmc_api_key" {
   secret_id = "prod/cmc_key"
 }
+
+data "aws_vpc" "dev-infrastructure-vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["dev-infrastructure-vpc"]
+  }
+}
+
+data "aws_subnets" "private-subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.dev-infrastructure-vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["dev-infrastructure-private-*"]
+  }
+}
+
+data "aws_subnet" "public-subnet1" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.dev-infrastructure-vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["dev-infrastructure-public-1"]
+  }
+}
+
+data "aws_subnet" "public-subnet2" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.dev-infrastructure-vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["dev-infrastructure-public-2"]
+  }
+}
+
