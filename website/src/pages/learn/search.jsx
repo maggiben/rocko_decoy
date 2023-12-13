53d4430058b068050f356957bc75ca5c2348e5cd
Vincent James <2112033+vinniejames@users.noreply.github.com>
2023-12-13 16:38:28 -0600

    search empty

--- website/src/pages/learn/search.jsx
+++ website/src/pages/learn/search.jsx
@@ -11,7 +11,7 @@ function SearchPage({ data, location }) {
   const posts = data.allMarkdownRemark.edges
 
   const [query, setQuery] = useState(location?.state?.query || '')
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState()
 
   const handleBackButton = () => {
     // eslint-disable-next-line no-unused-expressions
@@ -69,47 +69,47 @@ function SearchPage({ data, location }) {
       </section>
       <section className="!py-16">
         <div className="category_blogs_container space-y-5">
-          {results.length > 0 ? (
-            results.map(({ node }) => (
-              <Link
-                to={`/learn/${node.fields.slug}`}
-                key={node.id}
-                className="block"
-              >
-                <article className="category_blogs_content">
-                  <img
-                    src={node.frontmatter.coverUrl}
-                    alt="blog3"
-                    height={197}
-                    className="category_blogs_content_image"
-                  />
-                  <div className="category_blogs_content_details">
-                    <p className="category_blogs_content_details_p_1">
-                      {node.frontmatter.tags[0]}
-                    </p>
-                    <h2 className="category_blogs_content_details_h2_1">
-                      {node.frontmatter.title}
-                    </h2>
-                    <p className="category_blogs_content_details_p_2">
-                      {node.frontmatter.description}
-                    </p>
-                    <p className="category_blogs_content_details_p_3">
-                      {node.frontmatter.date}
-                    </p>
-                  </div>
-                </article>
-              </Link>
-            ))
-          ) : (
-            <h1 className="text-center">
-              We can&apos;t find what you&apos;re looking for. Try another
-              search or{' '}
-              <a style={{ textDecoration: 'underline' }} href="/">
-                go back
-              </a>
-              ?
-            </h1>
-          )}
+          {results.length > 0
+            ? results.map(({ node }) => (
+                <Link
+                  to={`/learn/${node.fields.slug}`}
+                  key={node.id}
+                  className="block"
+                >
+                  <article className="category_blogs_content">
+                    <img
+                      src={node.frontmatter.coverUrl}
+                      alt="blog3"
+                      height={197}
+                      className="category_blogs_content_image"
+                    />
+                    <div className="category_blogs_content_details">
+                      <p className="category_blogs_content_details_p_1">
+                        {node.frontmatter.tags[0]}
+                      </p>
+                      <h2 className="category_blogs_content_details_h2_1">
+                        {node.frontmatter.title}
+                      </h2>
+                      <p className="category_blogs_content_details_p_2">
+                        {node.frontmatter.description}
+                      </p>
+                      <p className="category_blogs_content_details_p_3">
+                        {node.frontmatter.date}
+                      </p>
+                    </div>
+                  </article>
+                </Link>
+              ))
+            : !!results && (
+                <h1 className="text-center">
+                  We can&apos;t find what you&apos;re looking for. Try another
+                  search or{' '}
+                  <a style={{ textDecoration: 'underline' }} href="/">
+                    go back
+                  </a>
+                  ?
+                </h1>
+              )}
         </div>
       </section>
     </Layout>
