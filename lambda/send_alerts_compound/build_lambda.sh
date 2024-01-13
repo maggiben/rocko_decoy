d79bfef544e0aee53d24bbd08233f70660a3b9de
Vinnie James <vinniejames@users.noreply.github.com>
2024-01-12 18:46:02 -0600

    Lambda build scripts (#304)
    * deploy lambda
    
    * update lambda build scripts
    
    ---------
    
    Co-authored-by: Vincent James <2112033+vinniejames@users.noreply.github.com>

--- lambda/send_alerts_compound/build_lambda.sh
+++ lambda/send_alerts_compound/build_lambda.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+ARCH=`uname -m`
+if [ $ARCH == "arm64" ]
+then
+  docker buildx build --platform linux/amd64 -t send_alerts_compound . --load
+  exit 0
+fi
+
 docker build -t  send_alerts_compound .
--- lambda/send_alerts_compound/push_docker.sh
+++ lambda/send_alerts_compound/push_docker.sh
@@ -3,10 +3,9 @@
 ARCH=`uname -m`
 if [ $ARCH == "arm64" ]
 then
-  echo "You dummy, this is an M1 machine!"
-  exit 1
+  echo "Warning, this is an M1 machine!"
 fi
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 835780150279.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 --profile rocko | docker login --username AWS --password-stdin 835780150279.dkr.ecr.us-east-1.amazonaws.com
 docker tag send_alerts_compound 835780150279.dkr.ecr.us-east-1.amazonaws.com/send_alerts_compound:latest
 docker push 835780150279.dkr.ecr.us-east-1.amazonaws.com/send_alerts_compound:latest
