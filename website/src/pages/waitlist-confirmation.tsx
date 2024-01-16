27c317ab492f17781238e5c8a8b04ab86fc453a1
adtomasz <112643291+adtomasz@users.noreply.github.com>
2024-01-16 21:25:33 +0530

    Add copied tooltip (#306)
    * Added copied alert on copy click in waitlist
    
    * Fixed tooltip position

--- website/src/pages/waitlist-confirmation.tsx
+++ website/src/pages/waitlist-confirmation.tsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from 'react'
 import Twitter from  '../assets/svg-icons/twitter.svg';
 import FacebookIcon from  '../assets/svg-icons/facebook.svg';
 import Thread from  '../assets/svg-icons/thread.svg';
-import Coppy from  '../assets/svg-icons/coppy.svg';
+import Copy from  '../assets/svg-icons/coppy.svg';
 import Linkedin from  '../assets/svg-icons/linkedin.svg';
 import { Link, navigate } from 'gatsby';
 import { FacebookShareButton, TwitterShareButton, LinkedinShareButton } from 'react-share';
 import copy from "copy-to-clipboard";
 import Layout from '../layout';
 import { ClipLoader } from 'react-spinners';
+import Tooltip from '../components/Tooltip';
 
 function WaitListConfirmation() {
     const url = "https://rocko.co";
     const [count, setCount] = useState(Number);
     const [loading, setLoading] = useState(true);
+    const [toolTip, setToolTip] = useState(false);
+    const [sectoolTip, setSecToolTip] = useState(false);
 
     const openThread = () => {
         window.open(`https://threads.net?url=${encodeURIComponent(url)}`, '_blank');
@@ -44,6 +47,21 @@ function WaitListConfirmation() {
       fetchData();
     }, []);
 
+  
+    const secTooltip = (e) => {
+        e.preventDefault()
+        setSecToolTip(true)
+        setTimeout(() => {
+            setSecToolTip(false);
+           }, 2000)
+    }
+    const openToolTip = (e) => {
+        e.preventDefault()
+        setToolTip(true)
+        setTimeout(() => {
+            setToolTip(false);
+           }, 2000);
+    }
   return (
     <>
  
@@ -65,7 +83,7 @@ function WaitListConfirmation() {
         </div>
         <div className='w-full text-center'>
             <p className='text-[#FFF] text-[16px] lg:text-[20px] !mb-[11px]'>Help spread the word!</p>
-            <ul className='flex  items-center justify-center  gap-x-2'>
+            <ul className='flex  items-center justify-center  gap-x-2 '>
                 <FacebookShareButton url={url}>
                     <li className='flex items-center justify-center lg:h-[48px] h-[40px] lg:w-[48px] w-[40px] rounded-full bg-[#ffffff1a] cursor-pointer mb-0'><Link to=''><FacebookIcon/></Link></li>
                 </FacebookShareButton>
@@ -79,18 +97,22 @@ function WaitListConfirmation() {
                 </LinkedinShareButton>
                 
                 <li className='flex items-center justify-center lg:h-[48px] h-[40px] lg:w-[48px] w-[40px] rounded-full bg-[#ffffff1a] cursor-pointer mb-0'><Link to='' onClick={openThread}><Thread/></Link></li>
-                <li className='flex items-center justify-center lg:h-[48px] h-[40px] lg:w-[48px] w-[40px] rounded-full bg-[#ffffff1a] cursor-pointer mb-0'><Link to='' onClick={copyToClipboard}><Coppy/></Link></li>
+                <li className='flex items-center justify-center lg:h-[48px] h-[40px] lg:w-[48px] w-[40px] rounded-full bg-[#ffffff1a] cursor-pointer mb-0 relative'>
+                    <Tooltip title="Copied" onClick={copyToClipboard}><Copy/></Tooltip>
+                </li>
+              
             </ul>
+          
         </div>
       </div>
       <div className='mt-[48px]'>
         <h6 className='text-[#141414] lg:text-[28px] text-[20px]   lg:mb-[32px] mb-[24px] font-medium'>Thanks for your support! </h6>
         <p className='text-[#141414] lg:text-[20px] text-[16px] pb-[20px] font-medium lg:leading-7 leading-6'>We’ll notify you as soon as you’re able to use Rocko. Please help us spread the word. The faster we grow, the more resources we can spend on improving Rocko and adding new features. </p>
         <div className='flex items-center justify-between gap-1 mt-[24px]'>
-            <div className='flex items-center  gap-4'>
+            <div className='flex items-center relative  gap-4'>
                 <Link to='' className=' max-w-[80px] w-full '><span className='text-[#141414] lg:text-[20px] text-[16px] block w-full md:border-e-2 md:pe-[20px] pe-[0] border-e-0 '>Share</span></Link>
                 <div>
-                <ul className='flex  items-center justify-center  gap-x-2 svg-icons'>
+                <ul className='flex  items-center justify-center  gap-x-2 svg-icons '>
                 <FacebookShareButton url={url}>
                     <li className='flex items-center justify-center lg:h-[48px] h-[35px] lg:w-[48px] w-[35px] rounded-full bg-[#EEE] cursor-pointer mb-0'><Link to=''><FacebookIcon className='text-black'/></Link></li>
                 </FacebookShareButton>
@@ -101,7 +123,10 @@ function WaitListConfirmation() {
                     <li className='flex items-center justify-center lg:h-[48px] h-[35px] lg:w-[48px] w-[35px] rounded-full bg-[#EEE] cursor-pointer mb-0'><Link to=''><Linkedin/></Link></li>
                 </LinkedinShareButton>
                 <li className='flex items-center justify-center lg:h-[48px] h-[35px] lg:w-[48px] w-[35px] rounded-full bg-[#EEE] cursor-pointer mb-0'><Link to='' onClick={openThread}><Thread/></Link></li>
-                <li className='flex items-center justify-center lg:h-[48px] h-[35px] lg:w-[48px] w-[35px] rounded-full bg-[#EEE] cursor-pointer mb-0'><Link to='' onClick={copyToClipboard}><Coppy/></Link></li>
+                <li className='flex items-center justify-center lg:h-[48px] h-[35px] lg:w-[48px] w-[35px] rounded-full bg-[#EEE] cursor-pointer mb-0 relative'>
+                    <Tooltip title="Copied" onClick={copyToClipboard}><Copy/></Tooltip>
+                </li>
+              
             </ul>
                 </div>
             </div>
