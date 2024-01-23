3cefcb2f28678d70693536e1979d1e6553a52696
Vincent James <2112033+vinniejames@users.noreply.github.com>
2024-01-23 16:33:40 -0600

    add sepolia USDC

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -8,7 +8,7 @@ export const USDCContract: { [key: number]: string } = {
   // See https://etherscan.io/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
   [chains.mainnet.id]: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
   [chains.goerli.id]: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
-  [chains.sepolia.id]: '',
+  [chains.sepolia.id]: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
 };
 
 // See https://docs.compound.finance/#networks
