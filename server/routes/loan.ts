5ecdcdbc3e3959b16a55caec806fb376d8a223c5
Vincent James <2112033+vinniejames@users.noreply.github.com>
2024-01-31 13:32:10 -0600

    auth loan routes

--- server/routes/loan.ts
+++ server/routes/loan.ts
@@ -7,8 +7,11 @@ import checkJwt from '../auth/checkJwt';
 /////////////////// Get loans
 // @ts-ignore
 router.get('/loans', checkJwt, (req, res, next) => {
+  // @ts-ignore
+  if (req.user.id === req.query.user) {
     let sql = `SELECT * FROM loans WHERE user_id = ?`;
-    const params = [req.query.user];
+    // @ts-ignore
+    const params = [req.user.id];
     // @ts-ignore
     db.query(sql, params, (err, results) => {
       if (err) {
@@ -17,6 +20,9 @@ router.get('/loans', checkJwt, (req, res, next) => {
       }
       res.status(200).json(results);
     })
+  } else {
+    return res.status(401).send('Unauthorized: Invalid ID');
+  }
 })
 
 
@@ -25,43 +31,49 @@ router.get('/loans', checkJwt, (req, res, next) => {
 router.post(
   // @ts-ignore
   '/add', checkJwt, (req, res, next) => {
-    let data = {
-      user_id: req.body.user,
-      transaction_hash: req.body.transaction_hash,
-      lending_protocol: req.body.lending_protocol,
-      loan_active: req.body.loan_active,
-      loan_asset: req.body.loan_asset,
-      principal_balance: req.body.outstanding_balance,
-      outstanding_balance: req.body.outstanding_balance,
-      collateral: req.body.collateral,
-      create_time: new Date(),
-      modified_time: new Date(),
-    };
-
-    if (!req.body.exist) { // if new loan on current user
-      let sql = "INSERT INTO loans SET ?";
-      // @ts-ignore
-      db.query(sql, data, (err, results) => {
-        if (err) {
-          console.error(err);
-          return next(new Error('Database query failed'));
-        }
-        res.send("Data successfully saved");
-      });
-    } else { // update loan (add borrowing and collateral)
-      let sql = "UPDATE loans SET transaction_hash = ?, outstanding_balance = ?, collateral = ?, modified_time = ? WHERE user_id = ?";
-
-      db.query(
-        sql, 
-        [data.transaction_hash, data.outstanding_balance, data.collateral, data.modified_time, data.user_id], 
+    // @ts-ignore
+    if (req.user.id === req.body.user) {
+      let data = {
         // @ts-ignore
-        (err, results) => {
-        if (err) {
-          console.error(err);
-          return next(new Error('Database query failed'));
-        }
-        res.send("Deposit Loan Status successfully updated");
-      });
+        user_id: req.user.id,
+        transaction_hash: req.body.transaction_hash,
+        lending_protocol: req.body.lending_protocol,
+        loan_active: req.body.loan_active,
+        loan_asset: req.body.loan_asset,
+        principal_balance: req.body.outstanding_balance,
+        outstanding_balance: req.body.outstanding_balance,
+        collateral: req.body.collateral,
+        create_time: new Date(),
+        modified_time: new Date(),
+      };
+
+      if (!req.body.exist) { // if new loan on current user
+        let sql = "INSERT INTO loans SET ?";
+        // @ts-ignore
+        db.query(sql, data, (err, results) => {
+          if (err) {
+            console.error(err);
+            return next(new Error('Database query failed'));
+          }
+          res.send("Data successfully saved");
+        });
+      } else { // update loan (add borrowing and collateral)
+        let sql = "UPDATE loans SET transaction_hash = ?, outstanding_balance = ?, collateral = ?, modified_time = ? WHERE user_id = ?";
+
+        db.query(
+          sql, 
+          [data.transaction_hash, data.outstanding_balance, data.collateral, data.modified_time, data.user_id], 
+          // @ts-ignore
+          (err, results) => {
+          if (err) {
+            console.error(err);
+            return next(new Error('Database query failed'));
+          }
+          res.send("Deposit Loan Status successfully updated");
+        });
+      }
+    } else {
+      return res.status(401).send('Unauthorized: Invalid ID');
     }
   }
 );
@@ -69,50 +81,59 @@ router.post(
 //////////////////// Update loan
 
 router.post("/update", checkJwt, (req, res, next) => {
-  const updateType = req.body.updateType;
+  // @ts-ignore
+  if (req.user.id) {
+    const updateType = req.body.updateType;
 
-  if (updateType === "repay") {
-    let data = {
-      id: req.body.id,
-      outstanding_balance: req.body.outstanding_balance,
-      interest: req.body.interest,
-      loan_active: req.body.loan_active,
-      transaction_hash: req.body.transaction_hash,
-      modified_time: new Date()
-    };
-    let sql = "UPDATE loans SET outstanding_balance = ?, interest = ?, loan_active = ?, transaction_hash = ?, modified_time = ? WHERE id = ?";
+    if (updateType === "repay") {
+      let data = {
+        // @ts-ignore
+        user_id: req.user.id,
+        id: req.body.id,
+        outstanding_balance: req.body.outstanding_balance,
+        interest: req.body.interest,
+        loan_active: req.body.loan_active,
+        transaction_hash: req.body.transaction_hash,
+        modified_time: new Date()
+      };
+      let sql = "UPDATE loans SET outstanding_balance = ?, interest = ?, loan_active = ?, transaction_hash = ?, modified_time = ? WHERE id = ? AND user_id = ?";
 
-    db.query(
-      sql, 
-      [data.outstanding_balance, data.interest, data.loan_active, data.transaction_hash, data.modified_time, data.id], 
-      // @ts-ignore
-      (err, results) => {
-      if (err) {
-        console.error(err);
-        return next(new Error('Database query failed'));
-      }
-      res.send("Amount and Active Status successfully updated");
-    });
+      db.query(
+        sql, 
+        [data.outstanding_balance, data.interest, data.loan_active, data.transaction_hash, data.modified_time, data.id, data.user_id], 
+        // @ts-ignore
+        (err, results) => {
+        if (err) {
+          console.error(err);
+          return next(new Error('Database query failed'));
+        }
+        res.send("Amount and Active Status successfully updated");
+      });
+    } else {
+      let data = {
+        // @ts-ignore
+        user_id: req.user.id,
+        id: req.body.id,
+        collateral: req.body.collateral,
+        transaction_hash: req.body.transaction_hash,
+        modified_time: new Date()
+      };
+      let sql = "UPDATE loans SET collateral = ?, transaction_hash = ?, modified_time = ? WHERE id = ? AND user_id = ?";
+
+      db.query(
+        sql, 
+        [data.collateral, data.transaction_hash, data.modified_time, data.id, data.user_id], 
+        // @ts-ignore
+        (err, results) => {
+        if (err) {
+          console.error(err);
+          return next(new Error('Database query failed'));
+        }
+        res.send("Buffer, Collateral and LiquidationPrice successfully updated");
+      });
+    }
   } else {
-    let data = {
-      id: req.body.id,
-      collateral: req.body.collateral,
-      transaction_hash: req.body.transaction_hash,
-      modified_time: new Date()
-    };
-    let sql = "UPDATE loans SET collateral = ?, transaction_hash = ?, modified_time = ? WHERE id = ?";
-
-    db.query(
-      sql, 
-      [data.collateral, data.transaction_hash, data.modified_time, data.id], 
-      // @ts-ignore
-      (err, results) => {
-      if (err) {
-        console.error(err);
-        return next(new Error('Database query failed'));
-      }
-      res.send("Buffer, Collateral and LiquidationPrice successfully updated");
-    });
+    return res.status(401).send('Unauthorized: Invalid ID');
   }
 });
 
