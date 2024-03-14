642c7bc2f087fbcde88bbaacaea8a3c964f2de42
Vincent James <2112033+vinniejames@users.noreply.github.com>
2024-03-13 20:50:32 -0500

    24 lambda

--- infrastructure/terraform/dev/us-east-1/lambda_get_daily_metrics.tf
+++ infrastructure/terraform/dev/us-east-1/lambda_get_daily_metrics.tf
@@ -61,6 +61,6 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_metrics" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.get_daily_metrics.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.lambda_every_5_minutes.arn
+  source_arn    = aws_cloudwatch_event_rule.lambda_every_24_hours.arn
 }
 
