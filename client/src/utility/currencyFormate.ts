23b1f1febf32768d249277d4a1e6811fca5c5531
Rockotom <138885903+Rockotom@users.noreply.github.com>
2024-04-16 11:20:48 -0400

    default 2 decimal places (#447)
    Co-authored-by: Thomas Harrison <thomasharrison@Thomass-Air-2.lan>

--- client/src/utility/currencyFormate.ts
+++ client/src/utility/currencyFormate.ts
@@ -1,39 +1,12 @@
-function formatCurrency(number: number): string {
-  // Convert the number to a string
-  const numStr: string = number.toString();
-
-  // Split the number into integer and decimal parts
-  const parts: string[] = numStr.split('.');
-  const integerPart: string = parts[0];
-  const decimalPart: string = parts.length > 1 ? parts[1] : '';
-
-  // Format the integer part with commas
-  let formattedInteger: string = '';
-  let count: number = 0;
-  for (let i = integerPart.length - 1; i >= 0; i--) {
-    const digit: string = integerPart[i];
-    if (count !== 0 && count % 3 === 0) {
-      formattedInteger = `,${formattedInteger}`;
-    }
-    formattedInteger = digit + formattedInteger;
-    count++;
-  }
-
-  // Pad the decimal part with zeros if necessary
-  const formattedDecimal: string = decimalPart.padEnd(2, '0');
-
-  // Combine integer and decimal parts
-  const formattedNumber: string =
-    formattedInteger + (decimalPart ? `.${formattedDecimal}` : '');
-
-  return formattedNumber;
-}
-
 export const shortenAddress = (address: string, count = 7) =>
   `${address.slice(0, count)}...${address.slice(-count)}`;
 
-export function financial(x: any, decimal = 0): string {
-  return formatCurrency(Number(parseFloat(x).toFixed(decimal)));
+export function financial(x: any, decimal = 2): string {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: decimal,
+    maximumFractionDigits: decimal,
+    useGrouping: true,
+  }).format(x);
 }
 
 export default financial;
